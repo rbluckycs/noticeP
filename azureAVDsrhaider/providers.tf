259c352ffd8d685a1bfaa12bee9ffa1df80ed235
Rizwan Haider <rbluckycsedoc@gmail.com>
2026-08-31 19:12:10 +0530

    azureSrhaider moved to git

---
+++ azureAVDsrhaider/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.12"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  resource_provider_registrations = "none"
+}
+
